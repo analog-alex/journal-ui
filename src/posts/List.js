41df586f4162dc5700fa89d74bbcb8af87dbf4ad
Miguel Alexandre <mumbleinout@gmail.com>
2021-03-14 16:56:03 +0000

    Add logic to fetch and list Posts
    Uses the axios library  to make the HTTP call.

---
+++ src/posts/List.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react'
+import { get } from '../api/Api.js'
+
+const List = (props) => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    get('posts')
+      .then(res => setPosts(res))
+      .catch(err => window.alert(`Sorry! There was an error: ${err.message}`))
+  }, [])
+
+  return (
+    <div>
+      <hr />
+      <ul>
+        {posts.map(post => <li key={post.id}>post</li>)}
+      </ul>
+    </div>
+  )
+}
+
+export default List
---
+++ src/posts/Posts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import List from './List.js'
+
+const Posts = (props) => {
+  return (
+    <div>
+      <h2>My blog posts!</h2>
+      <List />
+    </div>
+  )
+}
+
+export default Posts
